d61635763228d33ff8965cd211df68d788794260
Lina <kkmd@daum.net>
2015-11-03 00:56:50 +0900

    UserInfo update

--- Desktop/UserInfo.java
+++ Desktop/UserInfo.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap; 
-import java.util.StringTokenizer; 
+import java.util.StringTokenizer;
 import java.util.Vector;  
     
 public class UserInfo {   
